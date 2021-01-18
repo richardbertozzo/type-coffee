22c49b14e581c286eb2b6c630d9fd5e747564102
richardbertozzo <richardjrbertozzo@gmail.com>
2021-01-18 19:45:05 -0300

    feat: remove json properties

--- internal/entity/coffee.go
+++ internal/entity/coffee.go
@@ -2,7 +2,7 @@ package entity
 
 // Coffee represent the entity of a Coffee
 type Coffee struct {
-	UUID          string   `json:"uuid"`
-	Name          string   `json:"name"`
-	Caracteristic []string `json:"caracteristics"`
+	UUID          string
+	Name          string
+	Caracteristic []string
 }
