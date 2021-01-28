ab180bbd73b70ab60413ac6bb7d009c3b59bd3ac
Richard Bertozzo <richardjrbertozzo@gmail.com>
2021-01-27 22:09:21 -0300

    chore: add new methods in usecase and repo
    new methods:
    listbycaracteristic

--- coffee/interface.go
+++ coffee/interface.go
@@ -1,13 +1,25 @@
 package coffee
 
-import "github.com/richardbertozzo/type-coffee/coffee/entity"
+import (
+	"github.com/richardbertozzo/type-coffee/coffee/entity"
+)
+
+type Coffee struct {
+	UUID           string
+	Name           string
+	Image          string
+	Description    string
+	Caracteristics []string
+}
 
 type Repository interface {
 	GetByID(id string) (entity.Coffee, error)
+	ListByCaracteristic(entity.Caracteristic) ([]entity.Coffee, error)
 	Save(entity.Coffee) error
 }
 
 type UseCase interface {
-	GetByID(id string) (entity.Coffee, error)
-	Insert(entity.Coffee) error
+	GetByID(id string) (Coffee, error)
+	Create(Coffee) error
+	ListByCaracteristic(c string) ([]Coffee, error)
 }
