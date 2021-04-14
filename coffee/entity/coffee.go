013af9ad6fdda3cf07c681c9e2484a392f5b8561
Richard Bertozzo <richardjrbertozzo@gmail.com>
2021-04-14 16:39:06 -0300

    Ci (#5)
    * chore: add some useful make commands
    
    * ci: add upload coverage to codecov
    
    * lint: commented in export structs
    
    * lint: commented in exported func/structs

--- coffee/entity/coffee.go
+++ coffee/entity/coffee.go
@@ -18,14 +18,17 @@ func (c Coffee) String() string {
 	return fmt.Sprintf("ID: %s - Name: %s", c.ID, c.Name)
 }
 
+// IsZero returns if coffee is nil/empty
 func (c Coffee) IsZero() bool {
 	return c.ID == ""
 }
 
+// Caracteristics returns the caracteristics of coffee
 func (c Coffee) Caracteristics() []Caracteristic {
 	return c.caracteristics
 }
 
+// New create a new coffee value object
 func New(uuid, name, d string, l Link, c []Caracteristic) (Coffee, error) {
 	if name == "" {
 		return Coffee{}, errors.New("Name is blank")
