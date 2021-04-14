1b81243082bb95275442e089002b3dae91bb9170
Richard Bertozzo <richardjrbertozzo@gmail.com>
2021-04-14 16:25:38 -0300

    lint: commented in export structs

--- coffee/interface.go
+++ coffee/interface.go
@@ -4,6 +4,7 @@ import (
 	"github.com/richardbertozzo/type-coffee/coffee/entity"
 )
 
+// Coffee represents the Coffee entity with all characteristics
 type Coffee struct {
 	UUID           string
 	Name           string
@@ -12,20 +13,24 @@ type Coffee struct {
 	Caracteristics []string
 }
 
+// Repository the methods to write and read of Coffee (database abstraction)
 type Repository interface {
 	Reader
 	Writer
 }
 
+// Reader the reader methods to get coffee data
 type Reader interface {
 	GetByID(id string) (entity.Coffee, error)
 	ListByCaracteristic(entity.Caracteristic) ([]entity.Coffee, error)
 }
 
+// Writer the writer methods of coffee database
 type Writer interface {
 	Save(entity.Coffee) error
 }
 
+// UseCase the usecases/business rules layer
 type UseCase interface {
 	GetByID(id string) (Coffee, error)
 	Create(Coffee) error
