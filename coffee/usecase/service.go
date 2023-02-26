c8df7fd002b795c8cd4dd6db788f58424db22e76
Richard Bertozzo <richardjrbertozzo@gmail.com>
2023-02-26 20:13:01 -0300

    implement GetBestCoffees use case and call openapi provider

--- coffee/usecase/service.go
+++ coffee/usecase/service.go
@@ -1,97 +1,46 @@
 package usecase
 
 import (
+	"context"
 	"errors"
 
 	"github.com/richardbertozzo/type-coffee/coffee"
-	"github.com/richardbertozzo/type-coffee/coffee/entity"
 )
 
 type useCase struct {
-	db coffee.Repository
+	chatGPTProvider coffee.Provider
 }
 
 // NewService returns a new coffee use case
-func NewService(db coffee.Repository) coffee.UseCase {
+func NewService(chatGPTProvider coffee.Provider) coffee.UseCase {
 	return useCase{
-		db: db,
+		chatGPTProvider: chatGPTProvider,
 	}
 }
 
-func (u useCase) Create(c coffee.Coffee) error {
-	link, err := entity.NewImageLink(c.Image)
+func (u useCase) GetBestCoffees(ctx context.Context, filter coffee.Filter) (*coffee.BestCoffees, error) {
+	lenChar := len(filter.Characteristics)
+	if lenChar == 0 {
+		return nil, errors.New("must have minimum 1 characteristic input")
+	} else if lenChar > 3 {
+		return nil, errors.New("must have up to 3 characteristic input")
+	}
+
+	opts, err := u.chatGPTProvider.GetCoffeeOptionsByCharacteristics(ctx, filter)
 	if err != nil {
-		return err
-	}
-	cs, err := entity.NewCaracteristics(c.Caracteristics)
-	if err != nil {
-		return err
+		return nil, err
 	}
 
-	coff, err := entity.New(c.UUID, c.Name, c.Description, link, cs)
-	if err != nil {
-		return err
-	}
-	return u.db.Save(coff)
-}
-
-func (u useCase) GetByID(id string) (coffee.Coffee, error) {
-	if id == "" {
-		return coffee.Coffee{}, errors.New("id must be not blank")
+	options := make([]coffee.Option, len(opts))
+	for i, opt := range opts {
+		options[i] = coffee.Option{
+			Message: opt.Message,
+		}
 	}
 
-	c, err := u.db.GetByID(id)
-	if err != nil {
-		return coffee.Coffee{}, err
-	}
-
-	return convertCoffee(c), nil
-}
-
-func (u useCase) ListByCaracteristic(caStr string) (cos []coffee.Coffee, err error) {
-	ca, err := entity.NewCaracteristic(caStr)
-	if err != nil {
-		return []coffee.Coffee{}, err
-	}
-
-	coffees, err := u.db.ListByCaracteristic(ca)
-	if err != nil {
-		return []coffee.Coffee{}, err
-	}
-
-	for _, co := range coffees {
-		c1 := convertCoffee(co)
-		cos = append(cos, c1)
-	}
-
-	return cos, nil
-}
-
-func (u useCase) List() (cos []coffee.Coffee, err error) {
-	coffees, err := u.db.List()
-	if err != nil {
-		return []coffee.Coffee{}, err
-	}
-
-	for _, co := range coffees {
-		c1 := convertCoffee(co)
-		cos = append(cos, c1)
-	}
-
-	return cos, nil
-}
-
-func convertCoffee(c entity.Coffee) coffee.Coffee {
-	casStr := []string{}
-	for _, ca := range c.Caracteristics() {
-		casStr = append(casStr, ca.String())
-	}
-
-	return coffee.Coffee{
-		UUID:           c.ID,
-		Name:           c.Name,
-		Description:    c.Description,
-		Image:          c.Image.String(),
-		Caracteristics: casStr,
-	}
+	return &coffee.BestCoffees{
+		Characteristics: filter.Characteristics,
+		ChatGpt:         options,
+		Database:        nil,
+	}, nil
 }
