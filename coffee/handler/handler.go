82143adcdeb032e4193158f7967753fb65628689
Richard Bertozzo <rbertozzo@blockdaemon.com>
2023-02-26 16:46:24 -0300

    implement new function of openapi endpoint

--- coffee/handler/handler.go
+++ coffee/handler/handler.go
@@ -3,8 +3,6 @@ package handler
 import (
 	"net/http"
 
-	"github.com/go-chi/chi"
-	"github.com/go-chi/render"
 	"github.com/richardbertozzo/type-coffee/coffee"
 )
 
@@ -18,32 +16,7 @@ func NewHandler(s coffee.UseCase) *handlerHttp {
 	}
 }
 
-func (h *handlerHttp) ListCoffee(w http.ResponseWriter, r *http.Request) {
-	c, err := h.service.List()
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	render.Respond(w, r, c)
-}
-
-func (h *handlerHttp) GetCoffeeByID(w http.ResponseWriter, r *http.Request) {
-	param := chi.URLParam(r, "id")
-	if param == "" {
-		http.Error(w, "id must be not null", http.StatusBadRequest)
-		return
-	}
-
-	c, err := h.service.GetByID(param)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	render.Respond(w, r, c)
-}
-
-func (h *handlerHttp) CreateCoffee(w http.ResponseWriter, r *http.Request) {
-
+func (h *handlerHttp) GetBestTypeCoffee(w http.ResponseWriter, r *http.Request, params coffee.GetBestTypeCoffeeParams) {
+	//TODO implement me
+	panic("implement me")
 }
