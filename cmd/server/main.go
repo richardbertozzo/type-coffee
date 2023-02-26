1d1e1e3984b04ae84aaea408d5f51cfd1184c421
Richard Bertozzo <rbertozzo@blockdaemon.com>
2023-02-26 16:46:49 -0300

    use swagger to initiate the go app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,8 +5,11 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/go-chi/chi"
+	openapiMiddleware "github.com/deepmap/oapi-codegen/pkg/chi-middleware"
 	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+
+	"github.com/richardbertozzo/type-coffee/coffee"
 	"github.com/richardbertozzo/type-coffee/coffee/handler"
 	"github.com/richardbertozzo/type-coffee/coffee/repository"
 	"github.com/richardbertozzo/type-coffee/coffee/usecase"
@@ -18,18 +21,23 @@ func main() {
 		port = ":3000"
 	}
 
+	swagger, err := coffee.GetSwagger()
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	db := repository.NewMemoryDB()
 	uc := usecase.NewService(db)
 	h := handler.NewHandler(uc)
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/coffee", h.ListCoffee)
-	r.Get("/coffee/{id}", h.GetCoffeeByID)
-	r.Post("/coffee", h.CreateCoffee)
+	r.Use(openapiMiddleware.OapiRequestValidator(swagger))
+
+	coffee.HandlerFromMux(h, r)
 
 	log.Printf("http server runs on %s \n", port)
-	err := http.ListenAndServe(port, r)
+	err = http.ListenAndServe(port, r)
 	if err != nil {
 		log.Fatalf("error on start http server %v", err)
 	}
