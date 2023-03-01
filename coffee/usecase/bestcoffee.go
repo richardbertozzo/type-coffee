b6cb6d99e952fc1a0ef1032797108a9a74b6bb43
Richard Bertozzo <richardjrbertozzo@gmail.com>
2023-03-01 13:21:17 -0300

    add db service and default values, using goroutines to fetch

--- coffee/usecase/bestcoffee.go
+++ coffee/usecase/bestcoffee.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 
+	"golang.org/x/sync/errgroup"
+
 	"github.com/richardbertozzo/type-coffee/coffee"
 )
 
@@ -25,31 +27,65 @@ func New(chatGPTProvider, dbService coffee.Service) *useCase {
 func (u useCase) GetBestCoffees(ctx context.Context, filter coffee.Filter) (*coffee.BestCoffees, error) {
 	lenChar := len(filter.Characteristics)
 	if lenChar == 0 {
-		return nil, errors.New("must have minimum 1 characteristic input")
+		return nil, errors.New("must have minimum 1 input characteristic")
 	} else if lenChar > 3 {
-		return nil, errors.New("must have up to 3 characteristic input")
+		return nil, errors.New("must have up to 3 input characteristics")
 	}
 
-	opts, err := u.chatGPTProvider.GetCoffeeOptionsByCharacteristics(ctx, filter)
-	if err != nil {
-		return nil, err
-	}
+	filter.Limit = setDefaultLimit(filter.Limit)
 
-	if u.dbServiceEnabled {
-		// todo make it concurrent
-		u.dbService.GetCoffeeOptionsByCharacteristics(ctx, filter)
-	}
+	g, ctx := errgroup.WithContext(ctx)
 
-	options := make([]coffee.Option, len(opts))
-	for i, opt := range opts {
-		options[i] = coffee.Option{
-			Message: opt.Message,
+	var chatGptOpts []coffee.Option
+	g.Go(func() error {
+		opts, err := u.chatGPTProvider.GetCoffeeOptionsByCharacteristics(ctx, filter)
+		if err != nil {
+			return err
 		}
+
+		for _, opt := range opts {
+			chatGptOpts = append(chatGptOpts, coffee.Option{
+				Message: opt.Message,
+			})
+		}
+
+		return nil
+	})
+
+	var dbOpts []coffee.Option
+	if u.dbServiceEnabled {
+		g.Go(func() error {
+			opts, err := u.dbService.GetCoffeeOptionsByCharacteristics(ctx, filter)
+			if err != nil {
+				return err
+			}
+
+			for _, opt := range opts {
+				dbOpts = append(dbOpts, coffee.Option{
+					Message: opt.Message,
+				})
+			}
+
+			return nil
+		})
+
+	}
+
+	if err := g.Wait(); err != nil {
+		return nil, err
 	}
 
 	return &coffee.BestCoffees{
 		Characteristics: filter.Characteristics,
-		ChatGpt:         options,
-		Database:        nil,
+		ChatGpt:         chatGptOpts,
+		Database:        &dbOpts,
 	}, nil
+}
+
+func setDefaultLimit(limit int) int {
+	defaultLimit := 5
+	if limit == 0 || limit > 5 {
+		return defaultLimit
+	}
+	return limit
 }
