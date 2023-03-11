15efe8cdc54013bab1747c76d46d6703e0421d65
Richard Bertozzo <richardjrbertozzo@gmail.com>
2023-03-10 23:00:05 -0300

    add parser config wiht viper

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,12 +4,12 @@ import (
 	"context"
 	"log"
 	"net/http"
-	"os"
 
 	openapiMiddleware "github.com/deepmap/oapi-codegen/pkg/chi-middleware"
 	"github.com/getkin/kin-openapi/openapi3filter"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
+	"github.com/spf13/viper"
 
 	"github.com/richardbertozzo/type-coffee/coffee"
 	"github.com/richardbertozzo/type-coffee/coffee/handler"
@@ -18,21 +18,47 @@ import (
 	"github.com/richardbertozzo/type-coffee/infra/database"
 )
 
-func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = ":3000"
+type config struct {
+	Port       string
+	DBUrl      string
+	ChatGPTKey string
+}
+
+func loadConfig(filePath string) config {
+	portKey := "PORT"
+	dbKey := "DATABASE_URL"
+	chatGPTKey := "CHAT_GPT_KEY"
+
+	viper.SetConfigFile(filePath)
+	viper.SetDefault(portKey, ":3000")
+	if err := viper.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Fatalf("config not found: %s", err)
+		} else {
+			log.Fatalf("error in read config: %s", err)
+		}
 	}
-	chatGptKey := os.Getenv("CHAT_GPT_KEY")
+
+	chatGptKey := viper.GetString(chatGPTKey)
 	if chatGptKey == "" {
 		log.Fatal("CHAT_GPT_KEY ENV is required")
 	}
 
+	return config{
+		Port:       viper.GetString(portKey),
+		DBUrl:      viper.GetString(dbKey),
+		ChatGPTKey: viper.GetString(chatGPTKey),
+	}
+}
+
+func main() {
+	cfg := loadConfig(".env")
+
 	var db coffee.Service
-	dbURL := os.Getenv("DATABASE_URL")
-	if dbURL != "" {
+	if cfg.DBUrl != "" {
 		log.Println("database mode service enabled")
-		dbPool, err := database.NewConnection(context.Background(), dbURL)
+		dbPool, err := database.NewConnection(context.Background(), cfg.DBUrl)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -46,7 +72,7 @@ func main() {
 
 	swagger.Servers = nil
 
-	chatGPT, err := provider.NewChatGPTProvider(chatGptKey)
+	chatGPT, err := provider.NewChatGPTProvider(cfg.ChatGPTKey)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -70,8 +96,8 @@ func main() {
 
 	coffee.HandlerFromMux(h, r)
 
-	log.Printf("http server runs on %s \n", port)
-	err = http.ListenAndServe(port, r)
+	log.Printf("http server runs on %s \n", cfg.Port)
+	err = http.ListenAndServe(cfg.Port, r)
 	if err != nil {
 		log.Fatalf("error on start http server %v", err)
 	}
