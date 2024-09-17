fa892a5995c50e79be91a55f2dcf590ed2b75988
Richard Bertozzo <richardjrbertozzo@gmail.com>
2024-09-17 11:03:05 -0300

    add config function opts to db pool (#20)

--- infra/database/postgres.go
+++ infra/database/postgres.go
@@ -8,10 +8,25 @@ import (
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type OptFunc func(*pgxpool.Config)
+
+func WithMaxConn(maxConn int32) OptFunc {
+	return func(cfg *pgxpool.Config) {
+		cfg.MaxConns = maxConn
+	}
+}
+
+func WithMinConn(minConn int32) OptFunc {
+	return func(cfg *pgxpool.Config) {
+		cfg.MinConns = minConn
+	}
+}
+
 // NewConnection creates a new pooled connection for pgx
 func NewConnection(
 	ctx context.Context,
 	url string,
+	opts ...OptFunc,
 ) (
 	*pgxpool.Pool,
 	error,
@@ -22,11 +37,15 @@ func NewConnection(
 		return nil, fmt.Errorf("unable to parse connection string")
 	}
 
-	// pool settings
+	// pool settings defaults
 	cfg.MaxConns = 10
 	cfg.MinConns = 5
 	cfg.MaxConnLifetime = time.Minute * 10
 	cfg.MaxConnIdleTime = time.Minute * 5
+
+	for _, fn := range opts {
+		fn(cfg)
+	}
 
 	// connect
 	pool, err := pgxpool.NewWithConfig(ctx, cfg)
